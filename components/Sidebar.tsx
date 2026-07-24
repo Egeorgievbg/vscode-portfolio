8fb1cbe8783bb6c516baa1c3c86c4c9d2e8787a8
Evgeni Georgiev <153922151+Egeorgievbg@users.noreply.github.com>
2026-07-25 01:41:03 +0300

    Add services to primary sidebar navigation

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,15 +8,17 @@ import {
   VscCode,
   VscFiles,
   VscEdit,
+  VscTools,
 } from 'react-icons/vsc';
 
 import styles from '@/styles/Sidebar.module.css';
 
 const sidebarTopItems = [
   { Icon: VscFiles, path: '/' },
-  { Icon: VscGithubAlt, path: '/github' },
+  { Icon: VscTools, path: '/services' },
   { Icon: VscCode, path: '/projects' },
   { Icon: VscEdit, path: '/articles' },
+  { Icon: VscGithubAlt, path: '/github' },
   { Icon: VscMail, path: '/contact' },
 ];
 
@@ -32,7 +34,7 @@ const Sidebar = () => {
     <aside className={styles.sidebar}>
       <div className={styles.sidebarTop}>
         {sidebarTopItems.map(({ Icon, path }) => (
-          <Link href={path} key={path}>
+          <Link href={path} key={path} aria-label={path}>
             <div
               className={`${styles.iconContainer} ${
                 router.pathname === path && styles.active
@@ -54,7 +56,7 @@ const Sidebar = () => {
       <div className={styles.sidebarBottom}>
         {sidebarBottomItems.map(({ Icon, path }) => (
           <div className={styles.iconContainer} key={path}>
-            <Link href={path}>
+            <Link href={path} aria-label={path}>
               <Icon
                 fill={
                   router.pathname === path
